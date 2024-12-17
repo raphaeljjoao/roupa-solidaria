0b8a4618fe0ceb4cd5d721f0526d473cf07eda59
João Raphael <raphaeljoao02@gmail.com>
2024-12-17 03:42:58 -0300

    Change models structure

--- api/roupa_solidaria/api/models.py
+++ api/roupa_solidaria/api/models.py
@@ -1,15 +1,1 @@
-from django.db import models
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-class User(BaseModel):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    cpf = models.CharField(max_length=14)
-    address = models.TextField()
-    preferences = models.JSONField()
+from api.models import *
