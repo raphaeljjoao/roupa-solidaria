87717709b031448c400a74209e65b00acfb950d6
João Raphael <raphaeljoao02@gmail.com>
2024-12-17 03:09:52 -0300

    Create base app

--- api/roupa_solidaria/api/models.py
+++ api/roupa_solidaria/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+class User(BaseModel):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    cpf = models.CharField(max_length=14)
+    endereco = models.TextField()
+    preferencias = models.JSONField()
