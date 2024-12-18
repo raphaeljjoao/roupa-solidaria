a23bff087b2877665f996d9795c3ef46f1527de2
João Raphael <raphaeljoao02@gmail.com>
2024-12-18 05:11:44 -0300

    Refactor user model to extend AbstractUser

--- api/api/models/user.py
+++ api/api/models/user.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from . import BaseModel
 
-class User(BaseModel):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+class User(AbstractUser, BaseModel):
     cpf = models.CharField(max_length=14)
     address = models.TextField()
     preferences = models.JSONField()
+
+    def __str__(self):
+        return self.username
