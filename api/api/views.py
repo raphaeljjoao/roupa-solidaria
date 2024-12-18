f29e2d9ca460637594e7dbdb93d9653647d51870
João Raphael <raphaeljoao02@gmail.com>
2024-12-18 05:19:52 -0300

    Change views structure

--- api/api/views.py
+++ api/api/views.py
@@ -1,10 +1,1 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-# Create your views here.
-
-class TestView(APIView):
-    def get(self, request):
-        return Response(data={"message": "Hello, World!"}, status=status.HTTP_200_OK)
+from api.views import *
