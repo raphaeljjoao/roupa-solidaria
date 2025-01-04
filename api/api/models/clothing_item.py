0cf8e2b4202aca87c4d4298af33631f6ed416bb9
João Raphael <raphaeljoao02@gmail.com>
2025-01-04 19:16:58 -0300

    Create enums

--- api/api/models/clothing_item.py
+++ api/api/models/clothing_item.py
@@ -2,14 +2,50 @@ from django.db import models
 from .base_model import BaseModel
 from .user import User
 
+MALE = 'male'
+FEMALE = 'female'
+UNISSEX = 'unissex'
+    
+GENDER_CHOICES = (
+    (
+        (MALE, 'M'),
+        (FEMALE, 'F'),
+        (UNISSEX, 'U')
+    )
+)
+
+SMALL = 'S'
+MEDIUM = 'M'
+LARGE = 'L'
+EXTRA_LARGE = 'XL'
+
+SIZE_CHOICES = (
+    (SMALL, 'Small'),
+    (MEDIUM, 'Medium'),
+    (LARGE, 'Large'),
+    (EXTRA_LARGE, 'Extra Large')
+)
+
+SPRING = 'spring'
+SUMMER = 'summer'
+FALL = 'fall'
+WINTER = 'winter'
+
+SEASON_CHOICES = (
+    (SPRING, 'Spring'),
+    (SUMMER, 'Summer'),
+    (FALL, 'Fall'),
+    (WINTER, 'Winter')
+)
+
 class ClothingItem(BaseModel):
     donor_id = models.ForeignKey(User, on_delete=models.PROTECT)
     photo = models.BinaryField()
     description = models.TextField()
     color = models.CharField(max_length=50)
-    gender = models.CharField(max_length=20)
-    size = models.CharField(max_length=10)
-    season = models.CharField(max_length=20)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=20)
+    size = models.CharField(choices=SIZE_CHOICES, max_length=10)
+    season = models.CharField(choices=SEASON_CHOICES, max_length=20)
 
     def __str__(self):
         return f"{self.description} ({self.color}, {self.size})"
--- api/api/models/order.py
+++ api/api/models/order.py
@@ -1,7 +1,17 @@
 from django.db import models
 from . import BaseModel, User, ClothingItem
+from django.utils.translation import gettext_lazy as _
+from enum import Enum
+
+REQUESTED = 'requested'
+FINISHED = 'finished'
+
+STATUS_CHOICES = (
+    (REQUESTED, 'Requested'),
+    (FINISHED, 'Finished')
+)
 
 class Order(BaseModel):
     beneficiary_id = models.ForeignKey(User, on_delete=models.PROTECT)
     clothing_item_id = models.ForeignKey(ClothingItem, on_delete=models.CASCADE)
-    status = models.CharField(max_length=50)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50)
