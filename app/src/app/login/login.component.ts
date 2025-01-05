d7ef238bb0179765e95c7a7a0eba6c71faa7dd0a
João Raphael <raphaeljoao02@gmail.com>
2025-01-04 21:19:07 -0300

    Create basic login

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../service/login.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  constructor(
+    private loginService: LoginService,
+  ) {}
+
+  public getBaseUser() {
+    this.loginService.getUser(7).subscribe((data) => {
+      console.log(data);
+    });
+  }
 
 }
