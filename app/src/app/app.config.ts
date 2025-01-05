5c40c0aff48bbad47389951158d00370abc59e8e
João Raphael <raphaeljoao02@gmail.com>
2025-01-05 18:51:06 -0300

    Add snack bar

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -4,10 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations(),
+    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 3000}}
   ]
 };
--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LoginService } from '../../service/login.service';
 import { User } from '../../models/User';
 import { LocalStorageService } from '../../service/local-storage.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,17 @@ export class LoginComponent {
 
   constructor(
     private loginService: LoginService,
-    private localStorageService: LocalStorageService
+    private localStorageService: LocalStorageService,
+    private snackBar: MatSnackBar
   ) {}
 
   public getBaseUser() {
     this.loginService.getUser(8).subscribe((u) => {
       const user: User = u;
       this.localStorageService.login(user);
+      this.snackBar.open(`Logado com sucesso como o usuário ${user.username}`, '', {
+        panelClass: ['custom-snackbar', 'custom-snackbar-success']
+      });
     });
   }
 
