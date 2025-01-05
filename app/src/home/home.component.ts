8e9b51c246227a8d3fa2df3f6ce77659649bceda
João Raphael <raphaeljoao02@gmail.com>
2025-01-05 04:15:30 -0300

    Create home component

---
+++ app/src/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  loginPage() {
+    this.router.navigate(['/login']);
+  }
+
+}
