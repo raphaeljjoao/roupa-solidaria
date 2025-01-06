ca0b15e81fc4bd44fb9984677fbcc8f26b2185c7
João Raphael <raphaeljoao02@gmail.com>
2025-01-06 00:57:04 -0300

    Add user menu component to navbar

---
+++ app/src/app/shared/menu-options/menu-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-options',
+  imports: [],
+  templateUrl: './menu-options.component.html',
+  styleUrl: './menu-options.component.scss'
+})
+export class MenuOptionsComponent {
+
+}
--- app/src/app/shared/navbar/navbar.component.ts
+++ app/src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MenuOptionsComponent } from '../menu-options/menu-options.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, MenuOptionsComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  showMenu: boolean = false;
+
+  toggleMenu() {
+    console.log('toggleMenu', this.showMenu);
+    this.showMenu = !this.showMenu;
+  }
 
 }
