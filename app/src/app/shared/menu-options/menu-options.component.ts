df8e92e29b97465f3651c9f9c99fb3a6f02040e0
João Raphael <raphaeljoao02@gmail.com>
2025-01-06 01:01:16 -0300

    Logout function

--- app/src/app/shared/menu-options/menu-options.component.ts
+++ app/src/app/shared/menu-options/menu-options.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '../../../service/local-storage.service';
+import { Router } from '@angular/router';
+import { NotificationService } from '../../../service/notification-service.service';
 
 @Component({
   selector: 'app-menu-options',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './menu-options.component.scss'
 })
 export class MenuOptionsComponent {
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) { }
+
+  logout() {
+    this.localStorageService.logout();
+    this.router.navigate(['/']);
+    this.notificationService.showSuccess('Usuário deslogado com sucesso!');
+  }
 
 }
