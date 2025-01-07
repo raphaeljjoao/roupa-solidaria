c1f93ebaaed096a7a715976823e6aa421637fc24
João Raphael <raphaeljoao02@gmail.com>
2025-01-07 02:05:49 -0300

    Create not found page

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from '../home/home.component';
 import { ClothingListComponent } from './clothing-list/clothing-list.component';
 import { ClothingDonationComponent } from './clothing-donation/clothing-donation.component';
 import { ClothingDetailsComponent } from './clothing-details/clothing-details.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'list', component: ClothingListComponent },
   { path: 'donate', component: ClothingDonationComponent },
   { path: 'clothing/:id', component: ClothingDetailsComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
